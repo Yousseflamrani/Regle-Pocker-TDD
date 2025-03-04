d54df953494e388f8c192e4c10e45f4868752c2f
Yousseflamrani <yousseflamrani2001@gmail.com>
2025-03-04 15:19:54 +0100

    ajouter du deuxieme test pour testet l'égalite de deux carte

--- test/test_poker.py
+++ test/test_poker.py
@@ -12,3 +12,20 @@ def test_compare_one_pair_vs_high_card():
     result = compare_hand(hand_with_pair, hand_high_card)
 
     assert result == 1, "la carte avec la paire devrait ganger"
+
+
+def test_compare_higher_pair_wins():
+
+    """
+    testet qu'une paire de 9 bat une paire de 8
+
+    """
+
+    from src.pocker import compare_hand
+
+    pair_of_9 = "9H 9D 2S 5C 3H"
+    pair_of_8 = "8H 8D 2S 5C 3H"
+
+    result = compare_hand(pair_of_9, pair_of_8)
+
+    assert result == 1, "la caret 9 devrait battre la carte 8"
